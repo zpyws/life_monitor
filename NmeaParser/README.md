019e627c40117ad947dabd6e355e63dd2b1de662
yangwensen <yangws@landicorp.com>
2020-03-31 16:52:39 +0800

    [gps]NmeaParser porting to cy8c6347 [info]code=164880 sram=20900

---
+++ NmeaParser/README.md
@@ -0,0 +1,60 @@
+# NmeaParser
+
+This project demonstrate how to parser NMEA protocol.
+
+## How to use
+
+1. Include "nmeaparser.h" in your source file.
+2. Create a  nmeaparser: `struct nmea_parser parser[1];`.
+3. Init the `parser` with `nmea_parser_init(parser);`.
+4. Create your navigation report function: `void my_reporter(struct nav_data *navdata){...}`.
+5. Setup `parser`'s report funtion: `parser->report_nav_status = my_reporter;`.
+6. When read data from serialport or file, add thee data character by character to the parser with `nmea_parser_putchar(parser, c)`.
+7. When navigation data is ready, `parser` will trigger `my_reporter` function to report navigation status.
+
+## Modules
+
+| Module           | Files                         | Description                                 |
+| ---------------- | ----------------------------- | ------------------------------------------- |
+| `nmea_reader`    | `nmeardr.h` `nmeardr.c`       | Read NMEA data, contains one NMEA sentence. |
+| `nmea_tokenizer` | `nmeatknzr.h` `nmeatknzr.c`   | Split NMEA sentence into `token`s.          |
+| `parser`         | `nmeaparser.h` `nmeaparser.c` | Parse nmea from `token`s.                   |
+| `nav_data`       | `navdata.h` `navdata.c`       | Navigation data.                            |
+
+1. Add NMEA data to `nmea_reader` by character.
+2. If encounter '\n',  using `nmea_tokenizer` split `nmea_reader`.
+3. Parse `nmea_tokenizer` , and store results int `nav_data`.
+4. If encounter `GGA` sentence, print `nav_data`.
+
+## Navigation data
+
+#### Date and Time
+
+Date and time are in UTC.
+
+#### Latitude
+
+In degrees.
+
+Positive for North, and negative for South.
+
+#### Longitude
+
+In degrees.
+
+Positive for  East, and negative for West.
+
+#### Satellite's PRN and SVID
+
+`PRN` is the satellite's NO. in NMEA.
+
+`SVID` is the index in satellite's array.
+
+| SVID range | Constellation type |
+| ---------- | ------------------ |
+| 1-64       | GPS                |
+| 65-96      | GLONASS            |
+| 201-264    | Beidou             |
+
+
+
--- b02/neoway_gps_g7a.c
+++ b02/neoway_gps_g7a.c
@@ -1,13 +1,20 @@
 //created by yangwensen@20200330
 #include <rtthread.h>
 #include <rtdevice.h>
+
+#include "nmeaparser.h"
 //********************************************************************************************************************************************
-#define GPS_UART            "uart1"
-#define GPS_RX_BUFF_SIZE    512
+#define GPS_UART                "uart1"
+#define GPS_RX_BUFF_SIZE        512
+
+#define GPS_PRINT_RAW_DATA      0
 //********************************************************************************************************************************************
 static rt_device_t gps_uart_device = RT_NULL;
 static struct rt_semaphore rx_sem;
 static uint8_t gps_rx_buff[GPS_RX_BUFF_SIZE];
+//********************************************************************************************************************************************
+static int8_t gps_parser_init(void);
+static void gps_parse(uint8_t *buff, uint32_t len);
 //********************************************************************************************************************************************
 //by yangwensen@20200331
 static rt_err_t uart_rx_ind(rt_device_t dev, rt_size_t size)
@@ -67,6 +74,7 @@ static void task_gps(void *parameter)
 {
     rt_size_t res;
     
+    gps_parser_init();
     gps_init();
     while(1)
     {
@@ -76,19 +84,75 @@ static void task_gps(void *parameter)
         
         rt_sem_take(&rx_sem, RT_WAITING_FOREVER);
         
+    #if GPS_PRINT_RAW_DATA > 0
         gps_rx_buff[res] = 0;
         rt_kprintf("%s", gps_rx_buff);
-        rt_memset(gps_rx_buff, 0, sizeof(gps_rx_buff));
+    #endif
+        gps_parse(gps_rx_buff, res);
     }
 }
 //********************************************************************************************************************************************
 //by yangwensen@20200331
 extern void gps_startup(void)
 {
-    rt_thread_t thread = rt_thread_create("GPS", task_gps, RT_NULL, 512, 25, 10);
+    rt_thread_t thread = rt_thread_create("GPS", task_gps, RT_NULL, 1024, 25, 10);
     if (thread != RT_NULL)
     {
         rt_thread_startup(thread);
     }    
+}
+//********************************************************************************************************************************************
+struct nmea_parser parser[1];
+//********************************************************************************************************************************************
+//by yangwensen@20200331
+/*
+ * Print navigation data and wait for user's keystroke
+ * @navdata:    the navigation data
+ */
+void display_navdata(struct nav_data *navdata) {
+    rt_kprintf("NAVDATA:\n");
+    rt_kprintf("NAVDATA.FIX_VALID = %d\n", navdata->is_fixed);
+    rt_kprintf("NAVDATA.DATE = %d-%02d-%02d\n", navdata->date.year, navdata->date.month, navdata->date.day);
+    rt_kprintf("NAVDATA.TIME= %02d:%02d:%02d.%03d\n", navdata->time.hour, navdata->time.minute, navdata->time.second, navdata->time.ms);
+    rt_kprintf("NAVDATA.LAT = %.6f\n", navdata->lat);
+    rt_kprintf("NAVDATA.LON = %.6f\n", navdata->lon);
+    rt_kprintf("NAVDATA.ALT = %.2f\n", navdata->alt);
+    rt_kprintf("NAVDATA.HEADING = %.2f\n", navdata->heading);
+    rt_kprintf("NAVDATA.SPEED = %.2f\n", navdata->speed);
+    rt_kprintf("NAVDATA.HDOP = %.1f\n", navdata->hdop);
+    rt_kprintf("NAVDATA.VDOP = %.1f\n", navdata->vdop);
+    rt_kprintf("NAVDATA.PDOP = %.1f\n", navdata->pdop);
+    rt_kprintf("NAVDATA.NUM_SV_FIX = %d\n", navdata->sv_inuse);
+    rt_kprintf("NAVDATA.NUM_SV_VIEW = %d\n", navdata->sv_inview);
+
+    int svid;
+    for (svid = 0; svid < MAX_SVID; svid++) { 
+            struct sate *sate = navdata->sates + svid;
+            if (sate->valid)
+                    rt_kprintf("NAVDATA.SATE[%02d]: constell=%s prn=%02d, cn0=%02d, azim=%03d, elev=%02d, use=%d\n", 
+                                    svid, constell_name(sate->constell), sate->prn, sate->cn0, sate->azim, sate->elev, sate->in_use);
+    } 
+
+    rt_kprintf("\n");
+}
+//********************************************************************************************************************************************
+//by yangwensen@20200331
+static int8_t gps_parser_init(void)
+{
+    nmea_parser_init(parser);
+    parser->report_nav_status = display_navdata;
+    
+    return 0;
+}
+//********************************************************************************************************************************************
+//by yangwensen@20200331
+static void gps_parse(uint8_t *buff, uint32_t len)
+{
+    uint32_t i;
+    
+    for(i=0; i<len; i++)
+    {
+        nmea_parser_putchar(parser, buff[i]);
+    }
 }
 //********************************************************************************************************************************************
